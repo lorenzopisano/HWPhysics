8f5933466e0c6caf1999c0513382e9c411fa76dd
Lorenzo <lorenzo.pisano1994@gmail.com>
2025-07-21 14:44:21 +0200

    added UI

--- Assets/Scripts/BulletScript.cs
+++ Assets/Scripts/BulletScript.cs
@@ -2,16 +2,18 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    [SerializeField] float minY = 0f;
+    [SerializeField] float minY = -100.0f;
     [SerializeField] float maxX = 100f;
+    [SerializeField] float minX = -100f;
     void Start()
     {
-        
+        Debug.Log($"Y: {transform.position.y}, minY: {minY}");
+        Debug.Log($"X: {transform.position.x}, maxX: {maxX}, minX: {minX}");
     }
     
     void Update()
     {
-        if (this.transform.position.y < minY || this.transform.position.x > maxX)
+        if (this.transform.position.y < minY || this.transform.position.x > maxX || this.transform.position.x < minX)
         {
             Destroy(this.gameObject);
         }
